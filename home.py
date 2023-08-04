db9695ca276581275b8ecd8e86b5b2bc80139219
Ricardo Fernandes <67656688+Ricardojnf33@users.noreply.github.com>
2023-08-04 15:24:50 -0300

    Update home.py

--- home.py
+++ home.py
@@ -1,42 +1,38 @@
 import streamlit as st
-from moviepy.editor import VideoFileClip, concatenate_videoclips
-import tempfile
-import shutil
+from moviepy.editor import VideoFileClip
 
-# Function to create a reversed video and integrate it with the original video
-def reverse_and_double_video(input_file, output_file):
+# Function to create a reversed video
+def reverse_video(input_file, output_file):
     clip = VideoFileClip(input_file)
     reversed_clip = clip.fx(VideoFileClip.fx, lambda x: x.fl_time(-1))
-    final_clip = concatenate_videoclips([clip, reversed_clip.resize(2.0)])
-
-    final_clip.write_videofile(output_file, codec='libx264')
+    reversed_clip.write_videofile(output_file, codec='libx264')
 
 # Streamlit app
-st.title("Reverse and Double Video Web App")
+st.title("Reverse Video Web App")
 st.sidebar.header("Upload Video")
-uploaded_file = st.sidebar.file_uploader("Choose a video file", type=["mp4"])
+uploaded_file = st.sidebar.file_uploader("Escolha um arquivo de vídeo", type=["mp4"])
 
 if uploaded_file is not None:
     # Get the uploaded file name
     input_video_filename = uploaded_file.name
 
-    # Save the uploaded video to a temporary file
-    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-        temp_filename = temp_file.name
-        shutil.copyfileobj(uploaded_file, temp_file)
-
-    # Reverse the video, integrate with original, and save the combined video file
-    output_video_filename = "reversed_and_doubled_" + input_video_filename
-    reverse_and_double_video(temp_filename, output_video_filename)
+    # Reverse the video and save the reversed video file
+    output_video_filename = "reversed_" + input_video_filename
+    reverse_video(uploaded_file, output_video_filename)
 
     # Display the original video
-    st.subheader("Original Video")
+    st.subheader("Vídeo Original")
     st.video(uploaded_file)
 
-    # Display the reversed and doubled video
-    st.subheader("Reversed and Doubled Video")
+    # Display the reversed video
+    st.subheader("Vídeo Revertido")
+    st.video(output_video_filename)
+
+    # Offer the reversed video for download
+    st.subheader("Download Vídeo Revertido")
     with open(output_video_filename, "rb") as f:
-        reversed_doubled_video = f.read()
-    st.video(reversed_doubled_video)
+        reversed_video = f.read()
+    st.download_button("Clique aqui para baixar o vídeo revertido", data=reversed_video, file_name=output_video_filename, mime="video/mp4")
 
 st.markdown("Powered by Cheetah Data Science")
+
